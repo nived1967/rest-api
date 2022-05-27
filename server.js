47c6bf722fb51d208fd7f29bfd5821455d1be5eb
nived1967 <81697680+nived1967@users.noreply.github.com>
2022-05-27 19:41:43 +0530

    changed PORT again

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ const scoreboardRouter=require('./routes/scoreboard')
 app.use('/scoreboard',scoreboardRouter)
 
 
-app.listen(process.env.PORT,()=>console.log('Server Started'))
+app.listen(process.env.PORT || 3000,()=>console.log('Server Started'))
